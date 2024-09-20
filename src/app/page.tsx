a6b0b28ef0b6247d21f930ad197c3f2e1224571f
Harsh Gupta <guptaharsh940@gmail.com>
2024-09-20 17:23:01 +0530

    mobile opt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   
   return (
     <div className="flex">
-      <div className="flex min-w-full min-h-screen space-x-6 items-center justify-center">
+      <div className="flex flex-col md:flex-row space-y-6 min-w-full min-h-screen space-x-6 items-center justify-center ">
         <CategoriesChart />
         <DebitCreditChart />
       </div>
